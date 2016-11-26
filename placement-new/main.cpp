01756b439703eee86e9e2b58dccceab848e6ea73
Rikard Söderström <rikard.soderstrom@gmail.com>
2016-11-26 15:36:30 +0100

    [placement-new] Updated example to also run cppcheck

--- placement-new/main.cpp
+++ placement-new/main.cpp
@@ -12,18 +12,14 @@ int main (int argc, char *argv[])
   char *latest  = 0;
 
   // Allocate
-  std::cout << "mempool: " <<
-   static_cast<void*>(mempool) << std::endl;
+  printf("mempool: %p\n", mempool);
   for(int i=0; i < 100; i++)
   {
     if (offset + i*0x20000 < mempool_size)
     {
       latest = new (mempool+offset) char[i*0x20000];
       offset += i*0x20000;
-      std::cout <<
-        "i = "        << i                          <<
-        ", offset = " << offset                     <<
-        ", latest = " << static_cast<void*>(latest) << std::endl;
+      printf("i = %3d, offset = %8d, latest = %p\n", i, offset, latest);
     }
     else break;
   }
