4a36a4b395615a4473feece258281edd4628f860
Rikard Söderström <soderstrom.rikard@gmail.com>
2018-04-28 09:39:55 +0200

    simple auto-completion for custum vocabulary

--- main.cpp
+++ main.cpp
@@ -1,35 +1,111 @@
 #include <cstdio>
 #include <cstdlib>
+#include <memory>
 #include <string>
+#include <vector>
 
+namespace cmdline
+{
+/* shielding (us from) inclusions */
 #include <readline/readline.h>
 #include <readline/history.h>
+
+using string_vec = std::vector<std::string>;
+
+namespace /* anonymous */
+{
+static size_t     match_index = 0u;
+static string_vec matches;
+static string_vec vocabulary;
+static char *     line = nullptr;
+
+char* completion_generator(const char* text, int state)
+{
+    // This function is called with state=0 the first time; subsequent calls are
+    // with a nonzero state. state=0 can be used to perform one-time
+    // initialization for this completion session.
+
+    if (state == 0)
+    {
+        // During initialization, compute the actual matches for 'text' and keep
+        // them in a static vector.
+        matches.clear();
+        match_index = 0;
+
+        // Collect a vector of matches: vocabulary words that begin with text.
+        std::string textstr = std::string(text);
+        for (auto word : vocabulary)
+        {
+            if (word.size() >= textstr.size() && word.compare(0, textstr.size(), textstr) == 0)
+            {
+                matches.push_back(word);
+            }
+        }
+    }
+
+    if (match_index >= matches.size())
+    {
+        // We return nullptr to notify the caller no more matches are available.
+        return nullptr;
+    }
+    else
+    {
+        // Return a malloc'd char* for the match. The caller frees it.
+        return strdup(matches[match_index++].c_str());
+    }
+}
+
+char** completer(const char* text, int start, int end)
+{
+    (void)text;
+    (void)start;
+    (void)end;
+
+    // Note: returning nullptr here will make readline use the default filename
+    // completer.
+    return rl_completion_matches(text, completion_generator);
+    //return nullptr;
+}
+} //namespace: anonymous
+
+void initialize()
+{
+    vocabulary.push_back("info" );
+    vocabulary.push_back("read" );
+    vocabulary.push_back("write");
+
+    // bindings
+    rl_attempted_completion_function = completer;
+}
+
+int run()
+{
+    printf("Welcome! You can exit by pressing Ctrl+D at any time...\n");
+
+    while ((line = readline(">> ")) != nullptr)
+    {
+        if (strlen(line) > 0)
+        {
+            add_history(line);
+        }
+
+        printf("[%s]\n", line);
+
+        // readline malloc's a new buffer every time.
+        free(line);
+    }
+    return 0;
+}
+
+} //namespace: cmdline
+
 
 int main (int argc, char *argv[])
 {
     (void)argc;
     (void)argv;
-    printf("Welcome! You can exit by pressing Ctrl+C at any time...\n");
 
-    if (argc > 1 && std::string(argv[1]) == "-d")
-    {
-        // By default readline does filename completion. With -d, we disable this
-        // by asking readline to just insert the TAB character itself.
-        rl_bind_key('\t', rl_insert);
-    }
+    cmdline::initialize();
 
-    char* buf;
-    while ((buf = readline(">> ")) != nullptr) {
-        if (strlen(buf) > 0)
-        {
-            add_history(buf);
-        }
-
-        printf("[%s]\n", buf);
-
-        // readline malloc's a new buffer every time.
-        free(buf);
-    }
-
-    return 0;
+    return cmdline::run();
 }
