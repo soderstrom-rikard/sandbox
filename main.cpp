fa0913ed1573e1685de6875776e9184d9b2708ca
Rikard Söderström <soderstrom.rikard@gmail.com>
2019-06-12 22:03:38 +0200

    avoid declaring variables before it is actually used

--- main.cpp
+++ main.cpp
@@ -39,40 +39,40 @@ int main(int argc, char *argv[])
 {
     static_cast<void>(argc);
     static_cast<void>(argv);
-    SDL_Window *window;
+    SDL_Window   *window;
     SDL_Renderer *renderer;
-    SDL_Surface *surface;
-    SDL_Texture *texture;
-    int done;
-    SDL_Event event;
 
-    if (SDL_CreateWindowAndRenderer(0, 0, 0, &window, &renderer) < 0) {
-      SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
-          "SDL_CreateWindowAndRenderer() failed: %s", SDL_GetError());
-      return(2);
+    if (SDL_CreateWindowAndRenderer(0, 0, 0, &window, &renderer) < 0)
+    {
+      SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "SDL_CreateWindowAndRenderer() failed: %s", SDL_GetError());
+      return 2;
     }
 
-    surface = IMG_ReadXPMFromArray(const_cast<char**>(icon_xpm));
-    texture = SDL_CreateTextureFromSurface(renderer, surface);
-    if (!texture) {
-      SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
-          "Couldn't load texture: %s", SDL_GetError());
-      return(2);
+    SDL_Surface *surface = IMG_ReadXPMFromArray(const_cast<char**>(icon_xpm));
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+    if (!texture)
+    {
+      SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't load texture: %s", SDL_GetError());
+      return 2;
     }
     SDL_SetWindowSize(window, 800, 480);
 
-    done = 0;
-    while (!done) {
-      while (SDL_PollEvent(&event)) {
-        if (event.type == SDL_QUIT)
-          done = 1;
-      }
-      SDL_RenderCopy(renderer, texture, NULL, NULL);
-      SDL_RenderPresent(renderer);
-      SDL_Delay(100);
-    }
-    SDL_DestroyTexture(texture);
+    bool running = true;
+    while (running)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT) running = false;
+        }
 
+        SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+        SDL_RenderPresent(renderer);
+        SDL_Delay(100);
+    }
+
+    SDL_DestroyTexture(texture);
     SDL_Quit();
-    return(0);
+
+    return 0;
 }
