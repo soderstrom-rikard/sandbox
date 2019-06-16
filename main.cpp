0f4fe61895aef4a9a1d384e95fa72185e7f0f2e2
Rikard Söderström <soderstrom.rikard@gmail.com>
2019-06-17 00:17:51 +0200

    Added function for initalizing and drawing all element textures and positions

--- main.cpp
+++ main.cpp
@@ -66,6 +66,58 @@ SDL_Texture* String2Texture(const SDL_Color & color, const int32_t font_size, co
     return texture;
 }
 
+struct element_textures_t
+{
+    SDL_Texture * texture_atomic_number;
+    SDL_Texture * texture_symbol;
+    SDL_Texture * texture_name;
+    SDL_Texture * texture_atomic_weight;
+    SDL_Texture * texture_phase;
+    SDL_Texture * texture_category;
+    SDL_Texture * texture_occurrence;
+
+    SDL_Rect rect_atomic_number;
+    SDL_Rect rect_symbol;
+    SDL_Rect rect_name;
+    SDL_Rect rect_atomic_weight;
+    SDL_Rect rect_phase;
+    SDL_Rect rect_category;
+    SDL_Rect rect_occurrence;
+};
+
+bool initialize_element_textures(element_textures_t & et, const element_t & element, SDL_Renderer *renderer)
+{
+    bool ret = true;
+    if (et.texture_atomic_number = String2Texture(SDL_Color{255,255,255,255}, 16, std::to_string(element.atomic_number), renderer);et.texture_atomic_number == nullptr) ret = false;
+    if (et.texture_symbol        = String2Texture(SDL_Color{  0,  0,255,  0}, 16, element.symbol                       , renderer);et.texture_symbol        == nullptr) ret = false;
+    if (et.texture_name          = String2Texture(SDL_Color{  0,  0,  0,  0},  8, element.name                         , renderer);et.texture_name          == nullptr) ret = false;
+    if (et.texture_atomic_weight = String2Texture(SDL_Color{255,255,255,255}, 16, std::to_string(element.atomic_weight), renderer);et.texture_atomic_weight == nullptr) ret = false;
+    //if (et.texture_phase         = String2Texture(SDL_Color{255,255,255,255}, 16, std::to_string(element.phase        ), renderer);et.texture_phase         == nullptr) ret = false;
+    //if (et.texture_category      = String2Texture(SDL_Color{255,255,255,255}, 16, std::to_string(element.category     ), renderer);et.texture_category      == nullptr) ret = false;
+    //if (et.texture_occurrence    = String2Texture(SDL_Color{255,255,255,255}, 16, std::to_string(element.occurrence   ), renderer);et.texture_occurrence    == nullptr) ret = false;
+
+    et.rect_atomic_number = {20,  5, 50,  8};
+    et.rect_symbol        = {20, 20, 50, 50};
+    et.rect_name          = {10, 10, 50,  8};
+    et.rect_atomic_weight = {25, 30, 50,  8};
+    et.rect_phase         = {30, 40, 50,  8};
+    et.rect_category      = {40, 20, 50,  8};
+    et.rect_occurrence    = {50, 40, 50,  8};
+
+    return ret;
+}
+
+void draw_element_textures(element_textures_t & et, SDL_Renderer *renderer)
+{
+    SDL_RenderCopy(renderer, et.texture_atomic_number, nullptr, &et.rect_atomic_number);
+    SDL_RenderCopy(renderer, et.texture_symbol       , nullptr, &et.rect_symbol       );
+    SDL_RenderCopy(renderer, et.texture_name         , nullptr, &et.rect_name         );
+    SDL_RenderCopy(renderer, et.texture_atomic_weight, nullptr, &et.rect_atomic_weight);
+    SDL_RenderCopy(renderer, et.texture_phase        , nullptr, &et.rect_phase        );
+    SDL_RenderCopy(renderer, et.texture_category     , nullptr, &et.rect_category     );
+    SDL_RenderCopy(renderer, et.texture_occurrence   , nullptr, &et.rect_occurrence   );
+}
+
 int main(int argc, char *argv[])
 {
     static_cast<void>(argc);
@@ -83,11 +135,8 @@ int main(int argc, char *argv[])
 
     std::cout << "element name " << chemical_element.name << std::endl;
 
-    SDL_Texture* symbol_texture = String2Texture(SDL_Color{0,0,255,0}, 16, chemical_element.symbol, renderer);
-    SDL_Texture* name_texture   = String2Texture(SDL_Color{0,0,  0,0},  8, chemical_element.name  , renderer);
-
-    SDL_Rect symbol_rect = {20, 20, 50, 50};
-    SDL_Rect name_rect   = {10, 10, 50 , 8};
+    element_textures_t element_textures;
+    initialize_element_textures(element_textures, chemical_element, renderer);
 
     SDL_Surface *surface = IMG_ReadXPMFromArray(const_cast<char**>(icon_xpm));
     SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
@@ -108,8 +157,7 @@ int main(int argc, char *argv[])
         }
 
         SDL_RenderCopy(renderer, texture, nullptr, nullptr);
-        SDL_RenderCopy(renderer, symbol_texture, nullptr, &symbol_rect);
-        SDL_RenderCopy(renderer, name_texture  , nullptr, &name_rect);
+        draw_element_textures(element_textures, renderer);
         SDL_RenderPresent(renderer);
         SDL_Delay(100);
     }
